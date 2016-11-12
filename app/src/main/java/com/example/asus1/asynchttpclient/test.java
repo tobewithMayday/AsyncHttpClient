20cc5247ec0c078caaeccc16bac0d69a018b6338
tobewithMayday <405841911@qq.com>
2016-11-12 19:24:15 +0800

    this is addition

---
+++ app/src/main/java/com/example/asus1/asynchttpclient/test.java
@@ -0,0 +1,13 @@
+package com.example.asus1.asynchttpclient;
+
+/**
+ * Created by asus1 on 2016/11/12.
+ */
+public class test {
+    public test() {
+    }
+
+    public void mytest(){
+        System.out.print("mytest");
+    }
+}
